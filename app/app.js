e163530098da549d1ba27df9ee4878675227727d
Shan Batla <shan.batla@gmail.com>
2016-01-19 19:28:43 -0800

    Initialize and ste up ng-table.

--- app/app.js
+++ app/app.js
@@ -3,7 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.search'
+  'myApp.search',
+  'ngTable'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/search'});
--- app/search/search.js
+++ app/search/search.js
@@ -9,9 +9,7 @@ angular.module('myApp.search', ['ngRoute'])
   });
 }])
 
-.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
-
-  var searchResults;
+.controller('SearchCtrl', ['$scope', '$http', 'ngTableParams', function($scope, $http, ngTableParams) {
 
   $scope.searchForItem = function() {
     console.log('Searching for ' + $scope.searchInput);
@@ -21,10 +19,49 @@ angular.module('myApp.search', ['ngRoute'])
       url: 'http://api.vip.supplyhub.com:19000/products' + '?search=' + $scope.searchInput
     }).then(function (response) {
       console.log('Success: ' + response.data);
+      $scope.results = response.data;
     }, function (response) {
       console.log('Error: ' + response);
     });
 
   };
 
+  $scope.tableParams = new ngTableParams({}, {
+     dataset: $scope.results
+  });
+
 }]);
+
+
+
+
+//Notes
+  
+  //Response object from http response 
+// var obj = 
+// { 
+// brand: { name: 'HILTI, INC.', slug: 'hilti-inc' },
+//   product: 
+//    { pkgWeight: 2863,
+//      invoiceDescription: 'Hammer Drill Bit',
+//      pkgType: 'PK',
+//      indvWeightQtyUom: 'EA',
+//      upc: 613023533723,
+//      mfrCatNum: '00293416',
+//      psUomQty: 1,
+//      psUom: 'EA',
+//      pkgUom: 'EA',
+//      name: 'Hammer Drill Bit',
+//      pkgQty: 1,
+//      indvWeightUom: 'LB',
+//      fulltechDesc: 'Hammer Drill Bit',
+//      colThree: 506,
+//      pkgWeightUom: 'LB',
+//      indvWeightQty: 1,
+//      indvWeight: 2863,
+//      specificationSheetUrl: 'https://images.tradeservice.com/F5O2VZeQgqbz23ZI/ATTACHMENTS/DIR100129/LEVMFCE22208_1_2.pdf',
+//      slug: '613023533723' },
+//   category: 
+//    { name: 'Power Tools - Bits, Parts,  Accessories ',
+//      slug: 'power-tools-bits-parts-accessories-66150' } 
+// }
