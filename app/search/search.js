165cedc0e6000b0b54610aa4967b14a732df2de2
Shan Batla <shan.batla@gmail.com>
2016-01-19 18:42:28 -0800

    Implemented search funtionality.

--- app/search/search.js
+++ app/search/search.js
@@ -9,6 +9,20 @@ angular.module('myApp.search', ['ngRoute'])
   });
 }])
 
-.controller('SearchCtrl', [function() {
+.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
+
+  $scope.searchForItem = function() {
+    console.log('Searching for ' + $scope.searchInput);
+
+    $http({
+      method: 'GET',
+      url: 'http://api.vip.supplyhub.com:19000/products' + '?search=' + $scope.searchInput
+    }).then(function (response) {
+      console.log('Success' + response)
+    }, function (response) {
+      console.log('Error: ' + response);
+    });
+
+  };
 
 }]);
