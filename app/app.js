b80194eb82aab6bbf8483489978217aaec8ca286
Shan Batla <shan.batla@gmail.com>
2016-01-19 17:33:52 -0800

    Set up scaffold for app.

--- app/app.js
+++ app/app.js
@@ -3,10 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'myApp.search'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/search'});
 }]);
