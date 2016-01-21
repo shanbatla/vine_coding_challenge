a64046f68168aebcf44f15644964d375d0c4e6fa
Shan Batla <shan.batla@gmail.com>
2016-01-20 17:56:42 -0800

    Code clean up.

--- app/search/search.js
+++ app/search/search.js
@@ -12,7 +12,6 @@ angular.module('myApp.search', ['ngRoute', 'angularUtils.directives.dirPaginatio
 .controller('SearchCtrl', ['$scope', '$http', '$location', 'NgTableParams', function($scope, $http, $location, NgTableParams) {
 
   $scope.results = '';
-  $scope.paginatedResults = '';
 
   $scope.searchForItem = function() {
 
@@ -21,7 +20,6 @@ angular.module('myApp.search', ['ngRoute', 'angularUtils.directives.dirPaginatio
       url: 'http://api.vip.supplyhub.com:19000/products' + '?search=' + $scope.searchInput
     }).then(function (response) {
       $scope.results = response.data;
-      $scope.paginatedResults = $scope.results.slice(0, 10);
     }, function (response) {
       console.log('Error: ' + response);
     });
