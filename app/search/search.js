e68a6007d7d3e4f24a4f44a8a6d717f47b552e27
shanbatla <shan.batla@gmail.com>
2016-04-24 16:27:55 -0700

    Initial set up.

--- app/search/search.js
+++ app/search/search.js
@@ -9,20 +9,19 @@ angular.module('myApp.search', ['ngRoute', 'angularUtils.directives.dirPaginatio
   });
 }])
 
-.controller('SearchCtrl', ['$scope', '$rootScope', '$http', '$location', '$route', 'NgTableParams', function($scope, $rootScope, $http, $location, $route, NgTableParams) {
+.controller('SearchCtrl', ['$scope', '$http', '$location', '$route', 'NgTableParams', function($scope, $http, $location, $route, NgTableParams) {
 
-  // Instantiate scope variables
   $scope.results = '';
 
   $scope.searchForItem = function() {
 
     $http({
       method: 'GET',
-      url: 'http://api.vip.supplyhub.com:19000/products' + '?search=' + $scope.searchInput
+      url: 'https://api.github.com/repos/npm/npm/issues'
     }).then(function (response) {
       $scope.results = response.data;
 
-      $location.update_path('/?search=' + $scope.searchInput, true);
+      $location.update_path('/?search=' + $scope.searchInput, true);    
 
     }, function (response) {
       console.log('Error: ' + response);
