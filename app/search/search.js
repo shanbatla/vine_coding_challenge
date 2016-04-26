ef73ca71d534a319cce65d901757a1e0ec5ff14f
shanbatla <shan.batla@gmail.com>
2016-04-26 00:53:51 -0700

    Code clean up.

--- app/search/search.js
+++ app/search/search.js
@@ -2,13 +2,6 @@
 
 angular.module('myApp.search', ['ngRoute', 'angularUtils.directives.dirPagination'])
 
-// .config(['$routeProvider', function($routeProvider) {
-//   $routeProvider.when('/search', {
-//     templateUrl: 'search/search.html',
-//     controller: 'SearchCtrl'
-//   });
-// }])
-
 .controller('SearchCtrl', ['$scope', '$http', '$location', '$route', 'NgTableParams', function($scope, $http, $location, $route, NgTableParams) {
 
   $scope.results = '';
@@ -17,7 +10,7 @@ angular.module('myApp.search', ['ngRoute', 'angularUtils.directives.dirPaginatio
   $scope.searchForItem = function() {
     $http({
       method: 'GET',
-      url: 'https://api.github.com/repos/npm/npm/issues?page=1&per_page=100' //Note - max number of results API can serve is 100
+      url: 'https://api.github.com/repos/npm/npm/issues?page=1&per_page=100' // Note - max number of results API can serve is 100
     }).then(function (response) {
       $scope.results = response.data;
       $location.update_path('/?search=' + $scope.searchInput, true); 
