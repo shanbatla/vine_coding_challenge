4312edc5b00a013ec2253d32f18ec22f4dc135b1
shanbatla <shan.batla@gmail.com>
2016-04-25 18:50:46 -0700

    Issue page - fetching comments, still need to populate.  Also, code and comments clean ups.

--- app/issue/issue.js
+++ app/issue/issue.js
@@ -4,14 +4,31 @@ angular.module('myApp.issue', ['ngRoute', 'angularUtils.directives.dirPagination
 
 .controller('IssueCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
 
+  // Fetch issue number from route param
   var issueNumber = $routeParams.number;
 
+  // Fetch issue from GitHub API
   $http({
       method: 'GET',
       url: 'https://api.github.com/repos/npm/npm/issues/' + issueNumber
     }).then(function (response) {
       $scope.results = response.data;
       console.log($scope.results);
+
+      // If comments exist, get them
+      if(response.data.comments > 0) {
+
+        // Fetch comments
+        $http({
+            method: 'GET',
+            url: response.data.comments_url
+        }).then(function (response) {
+            console.log(response.data);
+        }, function (response) {
+          console.log('Error: ' + response);    
+          });
+
+      }
 
     }, function (response) {
       console.log('Error: ' + response);
--- app/search/search.js
+++ app/search/search.js
@@ -13,16 +13,14 @@ angular.module('myApp.search', ['ngRoute', 'angularUtils.directives.dirPaginatio
 
   $scope.results = '';
 
+  // Fetch issues
   $scope.searchForItem = function() {
-
     $http({
       method: 'GET',
       url: 'https://api.github.com/repos/npm/npm/issues?page=1&per_page=100' //Note - max number of results API can serve is 100
     }).then(function (response) {
       $scope.results = response.data;
-
       $location.update_path('/?search=' + $scope.searchInput, true); 
-
     }, function (response) {
       console.log('Error: ' + response);
     });
