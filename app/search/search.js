217f8d36b74329b03e0116ab3908b0e1eff0d404
shanbatla <shan.batla@gmail.com>
2016-04-26 18:08:39 -0700

    Truncate body of issue to 140 chars and end on clean word.  Also, added comments.

--- app/search/search.js
+++ app/search/search.js
@@ -13,10 +13,19 @@ angular.module('myApp.search', ['ngRoute', 'angularUtils.directives.dirPaginatio
       url: 'https://api.github.com/repos/npm/npm/issues?page=1&per_page=100' // Note - max number of results API can serve is 100
     }).then(function (response) {
       $scope.results = response.data;
+
     }, function (response) {
       console.log('Error: ' + response);
     });
   
   };
+
+  // Truncate the body of the issue if its over 140 chars and end on a clean word
+  $scope.truncateBody = function(body) {
+    var trimmedString = body.substr(0, 140);
+
+    //re-trim if we are in the middle of a word
+    return trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")));
+  }
 
 }]);
